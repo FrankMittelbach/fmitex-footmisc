dd5d1954ad4d0b3da4ca1cb3b3309ec951d472fa
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-02-02 00:12:10 +0100

    provide new options and rough interface for general customization

--- update-all.sh
+++ update-all.sh
@@ -5,5 +5,12 @@ l3build save \
 	footmisc-003 \
 	footmisc-004 \
 	footmisc-005 \
-	footmisc-006
+	footmisc-006 \
+	footmisc-floats-000 \
+	footmisc-floats-001 \
+	footmisc-floats-002 \
+	footmisc-floats-003 \
+	footmisc-floats-004 \
+	footmisc-floats-005
+
 
