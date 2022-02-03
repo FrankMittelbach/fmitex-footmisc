9dbcdb811161cfdea6603c9fae805f23642fdd0f
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-02-03 17:31:26 +0100

    cleanup and documentation (WIP)

--- update-all.sh
+++ update-all.sh
@@ -12,6 +12,27 @@ l3build save \
 	footmisc-floats-003 \
 	footmisc-floats-004 \
 	footmisc-floats-005 \
-	footmisc-symbols-001
+	footmisc-symbols-001 \
+	footmisc-symbols-002 \
+	footmisc-floats-latex \
+	footmisc-floats-bottom \
+	footmisc-floats-bottomfloats \
+	footmisc-floats-bottomfootnotes \
+	footmisc-floats-abovefloats \
+	footmisc-floats-belowfloats \
+	footmisc-floats-bottom-abovefloats \
+	footmisc-floats-bottom-belowfloats \
+	footmisc-floats-latex-flushbottom \
+	footmisc-floats-bottom-flushbottom \
+	footmisc-floats-bottomfloats-flushbottom \
+	footmisc-floats-bottomfootnotes-flushbottom \
+	footmisc-floats-abovefloats-flushbottom \
+	footmisc-floats-belowfloats-flushbottom \
+	footmisc-floats-bottom-abovefloats-flushbottom \
+	footmisc-floats-bottom-belowfloats-flushbottom
+
+
+
+
 
 
