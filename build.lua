78ab55e1bb0ed961915614614628713e719ed306
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-02-10 21:45:46 +0100

    ready for CTAN

--- build.lua
+++ build.lua
@@ -13,6 +13,25 @@ checkengines = {"pdftex"}
 
 checkruns     = 2
 
+
+-- Upload meta data
+
+uploadconfig = {
+ pkg = "footmisc",
+ version = "v6.0a 2022-02-10",
+ author = "Frank Mittelbach",
+ license = "lppl1.3c",
+ summary = "A range of footnote options",
+ ctanPath = "/macros/latex/contrib/footmisc",
+ repository = "https://github.com/frankmittelbach/fmitex-footmisc",
+ bugtracker = "https://github.com/FrankMittelbach/fmitex-footmisc/issues",
+ uploader = "Frank Mittelbach",
+ email = "frank.mittelbach@latex-project.org",
+ update = true ,
+}
+
+
+
 if not release_date then
    kpse.set_program_name("kpsewhich")
    dofile(kpse.lookup("l3build.lua"))
