94d574f1e1066baaf25f939225512c974564ff3b
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-02-14 11:17:45 +0100

    record version in build.lua

--- build.lua
+++ build.lua
@@ -18,7 +18,7 @@ checkruns     = 2
 
 uploadconfig = {
  pkg = "footmisc",
- version = "v6.0a 2022-02-10",
+ version = "v6.0b 2022-02-14",
  author = "Frank Mittelbach",
  license = "lppl1.3c",
  summary = "A range of footnote options",
